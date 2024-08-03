5dee7c9451fb3e9f4cf92fdd83706576e77cc88c
sachin <sachinmvcalicut@gmail.com>
2024-08-03 22:03:07 +0530

    initial commit

--- reverse_of_a_number/src/Main.java
+++ reverse_of_a_number/src/Main.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("reverse of a number");
-        System.out.println("enter which number you want to reverse");
-        Scanner sc=new Scanner(System.in);
-        int x= sc.nextInt();
-        int y=0,rev=0;
+        int number = getNumberFromKeyboard();
+        int rev = getReverse(number);
+        System.out.println("Reverse : " + rev);
+    }
 
-        while(x>0){
-        y=x%10;
-        rev=rev*10+y;
-        x=x/10;
+    static int getNumberFromKeyboard() {
+        System.out.println("Enter which number you want to reverse : ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
 
+    static int getReverse(int number) {
+        int rev = 0;
+        while (number > 0) {
+            rev = rev * 10 + (number % 10);
+            number = number / 10;
         }
-        System.out.println("reverse :"+rev);
-
+        return rev;
     }
 }
